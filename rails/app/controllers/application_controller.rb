031ea31a58f98dc76520c050e056538edac0c940
meitantei13 <or.soccer.13@gmail.com>
2025-11-19 17:13:57 +0900

    rubocopの修正

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -17,6 +17,6 @@ class ApplicationController < ActionController::API
     end
 
     def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end
