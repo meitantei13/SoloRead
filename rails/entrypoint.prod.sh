18abbf0e738ca109f190db901794fe1e56a6591b
meitantei13 <or.soccer.13@gmail.com>
2025-12-25 11:35:38 +0900

    seedをコメントアウト

--- rails/entrypoint.prod.sh
+++ rails/entrypoint.prod.sh
@@ -13,7 +13,7 @@ echo "bundle exec rails db:migrate RAILS_ENV=production"
 bundle exec rails db:migrate RAILS_ENV=production
 
 echo "bundle exec rails db:seed RAILS_ENV=production"
-bundle exec rails db:seed RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
 
 echo "exec pumactl start"
 bundle exec pumactl start
