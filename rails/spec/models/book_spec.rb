f7de3d1b2c91ebe4d8e1e8b4bdf1a9ab26159926
meitantei13 <or.soccer.13@gmail.com>
2026-01-05 23:42:30 +0900

    rubocopの修正

--- rails/spec/models/book_spec.rb
+++ rails/spec/models/book_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Book, type: :model do
   include ActiveSupport::Testing::TimeHelpers
-  
+
   context "factoryのデフォルト設定に従ったとき" do
     subject { create(:book) }
 
