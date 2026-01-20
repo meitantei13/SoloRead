47320c5f48d64bec5efce91a0efb53d3476b064d
meitantei13 <or.soccer.13@gmail.com>
2026-01-21 01:05:37 +0900

    テストデータにジャンルを追加

--- rails/app/services/guest_sample_data/draft_samples.rb
+++ rails/app/services/guest_sample_data/draft_samples.rb
@@ -3,7 +3,7 @@ module GuestSampleData
     DATA = [
       { title: "「好き」を言語化する技術", author: "三宅 香帆" },
       { title: "努力の地図", author: "荒木 博行" },
-      { title: "世界の一流は「休日」に何をしているのか", author: "越川 慎司" },
+      { title: "世界の一流は「休日」に何をしているのか", author: "越川 慎司", genre: "ビジネス" },
     ].freeze
   end
 end
