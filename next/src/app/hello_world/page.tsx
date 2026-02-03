b645fadddeab31f3fa259cd72b42b3309ad9b048
meitantei13 <or.soccer.13@gmail.com>
2026-02-04 07:01:35 +0900

    muiの導入

---
+++ next/src/app/hello_world/page.tsx
@@ -0,0 +1,5 @@
+export default function HelloWorld() {
+  const count = 100;
+
+  return <div>Hellow World!! / {count}</div>;
+}
--- next/src/pages/hello_world.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-
-const HelloWorld: NextPage = () => {
-  const count = 100;
-
-  return <div>Hellow World!! / {count}</div>;
-};
-
-export default HelloWorld;
