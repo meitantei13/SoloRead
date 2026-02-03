95a01c7e4dda752316b3dea2bf2cc198277cbf07
meitantei13 <or.soccer.13@gmail.com>
2026-02-04 05:31:40 +0900

    ciのテスト

--- next/src/pages/hello_world.tsx
+++ next/src/pages/hello_world.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const HelloWorld: NextPage = () => {
   const count = 100;
 
-  return <div>Hellow World! / {count}</div>;
+  return <div>Hellow World!! / {count}</div>;
 };
 
 export default HelloWorld;
