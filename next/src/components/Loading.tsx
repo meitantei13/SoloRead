6f003211a114bd4055ad0d0b55e7c659e32a21ba
meitantei13 <or.soccer.13@gmail.com>
2026-03-24 06:56:22 +0900

    priority を追加してLCPを改善

--- next/src/components/Loading.tsx
+++ next/src/components/Loading.tsx
@@ -12,7 +12,13 @@ export default function Loading() {
         alignItems: "center",
       }}
     >
-      <Image src="/loading.svg" width={150} height={150} alt="loading・・・" />
+      <Image
+        src="/loading.svg"
+        width={150}
+        height={150}
+        alt="loading・・・"
+        priority
+      />
     </Box>
   );
 }
